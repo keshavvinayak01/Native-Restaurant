59a3948ccf704413918ed571d1758aa59acae381
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-02-26 00:52:09 +0530

    Native Navigation-2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import Menu from  './MenuComponent';
-import {DISHES} from '../shared/dishes';
+import Home from './HomeComponent';
 import {DishDetail} from './components/DishDetailComponent';
 import {View} from 'react-native';
 import { createStackNavigator } from 'react-navigation';
@@ -20,12 +20,44 @@ const MenuNavigator  = createStackNavigator({
                 }
         }
 })
+const HomeNavigator = createStackNavigator({
+       Home : { screen : Home}
+},{
+        navigationOptions : {
+                headerStyle : {
+                        backgroundColor : '#512DA8'
+                },
+                headerTintColor : '#fff',
+                headerTitleStyle : {
+                        color : '#BADA55'
+                }
+        }
+});
+
+const MainNavigator = createDrawNavigator({
+        Home : {
+                screen : HomeNavigator,
+                navigationOptions : {
+                        title : 'Home',
+                        drawerLabel : 'Home'
+                }
+        },
+        Menu : {
+                screen : MenuNavigator,
+                navigationOptions : {
+                        title : 'Menu',
+                        drawerLabel : 'Menu'
+                }
+},{
+        drawerBackgroundColor : '#D1C4E9'
+});
+
 class Main extends Component { 
         render(){
                 return(
                         <View style={{flex:1,
                         paddingTop : Platform.Os==='ios' ? 0 : Expo.Constants.statusBarHeight}}>
-                                <MenuNavigator />
+                                <MainNavigator />
                         </View>
                                  );
         }
