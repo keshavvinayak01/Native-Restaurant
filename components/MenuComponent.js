75c4a2bedd23914afa40dfcedae5e4d7240c6799
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-02-25 23:40:02 +0530

    Components Part-1

---
+++ components/MenuComponent.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import {View, FlatlList} from 'react-native';
+import {ListItem} from 'react-native-elements';
+
+function Menu(props){
+
+        const renderMenuItem = ({item,index}) => {
+                return(
+                        <ListItem
+                        key={index}
+                        title={item.name}
+                        subtitle={item.description}
+                        hideChevron={true}
+                        leftAvatar={{source: require('./images/uthappizza.png')}}
+                        />
+                )
+        }
+
+        return(
+                <FlatlList
+                        data={props.dishes}
+                        renderItem = {renderMenuItem}
+                        keyExtractor={item => item.id.toString()}
+                />
+        )
+}
+
+export default Menu
